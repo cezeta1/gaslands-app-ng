c38425108e736130a038cfa4e842bd49c1717a00
GitHub Action <action@github.com>
2025-04-04 08:07:32 +0000

    deploy files

--- src/app/core/services/browser-storage/encrypt.utils.ts
+++
@@ -1,4 +0,0 @@
-// import CryptoJS, { AES } from 'crypto-js';
-
-// export const encryptAES = (data: string, key: string) => AES.encrypt(JSON.stringify(data), key).toString();
-// export const decryptAES = (cipher: string, key: string) => AES.decrypt(cipher, key).toString(CryptoJS.enc.Utf8);
