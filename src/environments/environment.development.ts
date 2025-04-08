cfc74dfd821b9dafa7255bd74048296805b49d8b
Julian Czerweny <julian.czerweny@nexpay.com.au>
2025-04-09 09:39:28 +1000

    updated style

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,7 @@ import { Environment } from "./env.interface";
 
 export const environment: Environment = {
   production: false,
+  rootUrl: 'https://cezeta1.github.io/',
   links: {
     email: 'julian.czerweny@gmail.com',
     github: 'https://github.com/cezeta1',
